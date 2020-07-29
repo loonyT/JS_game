c79bcc61fc370db4672f926e83c4b539052e5a3e
Baslash94 <bastien.deresteau@gmail.com>
2020-07-29 21:30:46 +0200

    ive added the setPreFight function and the setFight function

---
+++ enemy.js
@@ -0,0 +1,14 @@
+let enemy;
+
+function Enemy (enemyType, health, mana, strenght, agility, speed) {
+    
+    this.ennemyType= enemyType;
+    this.health = health;
+    this.mana = mana;
+    this.strenght = strenght;
+    this.agility = agility;
+    this.speed = speed;
+    
+    
+
+}
--- gameManager.js
+++ gameManager.js
@@ -40,24 +40,56 @@ let GameManager = {
 
         }
 
-let getInterface = document.querySelector(".interface");
-getInterface.innerHTML =    ' <img src="https://image.shutterstock.com/image-photo/saint-petersburg-russia-august-1-260nw-307233971.jpg" '+ classType.toLowerCase()  + '    "  class="img-avatar"> <div> <h3> ' + classType   + '</h3>'     ;
-        
-        
-        
-        
-        
+        let getInterface = document.querySelector(".interface");
+        getInterface.innerHTML = ' <img src="https://image.shutterstock.com/image-photo/saint-petersburg-russia-august-1-260nw-307233971.jpg" ' + classType.toLowerCase() + '    "  class="img-avatar"> <div> <h3> ' + classType + '</h3><p>Health: ' + player.health + '</p><p>Mana: ' + player.mana + '</p><p>Strenght: ' + player.strenght + '</p> <p>Agility: ' + player.agility + '</p><p>Speed: ' + player.speed + '</p></div>';
+
+
+
+
+
     },
 
 
     setPreFight: function () {
 
 
+        let getHeader = document.querySelector(".header");
+        let getActions = document.querySelector(".actions");
+        let getArena = document.querySelector(".arena");
+        getHeader.innerHTML = '<p>Task: Find an enemy</p>';
+        getActions.innerHTML = '<a href = "#" class = "btn-prefight" onclick = "GameManager.setFight"> Search for  enemy! </a>';
+        getArena.style.visibility = "visible";
+    },
+
+    setFight: function () {
+
+        let getHeader = document.querySelector(".header");
+        let getActions = document.querySelector(".actions");
+        let getEnemy = document.querySelector(".enemy");
+        // Create enemy ! 
+        let enemy00 = new Enemy("Goblin", 100, 0, 50, 100, 100);
+        let enemy01 = new Enemy("Troll", 200, 0, 150, 80, 150);
+        let chooseRandomEnemy = Math.floor(Math.random() * Math.floor(2)); //choose a number between one and zero. 
+        switch (chooseRandomEnemy) {
+
+            case 0:
+                enemy = enemy00;
+                break;
+
+            case 1:
+                enemy = enemy01;
+                break;
+
+        }
+
+        getHeader.innerHTML = '<p>Task : choose your move </p>';
+        getActions.innerHTML = '<a href = "#" class = "btn-prefight" onclick = "PlayerMoves.calcAttack()"> Attack! </a>';
+        getEnemy.innerHTML = '<img src="https://image.shutterstock.com/image-vector/rpg-videogame-characters-enemies-600w-1208517286.jpg' + enemy.enemyType.toLowerCase()+ ' " alt = "' + enemy.enemyType + ' " class = "img-avatar" > <div><h3>' + enemy.enemyType + '</h3><p class ="health-enemy">Health: ' + enemy.health + '</p><p>Mana : ' + enemy.mana + '</p><p>Strenght: ' + enemy.strenght + '</p><p>Agility: ' +enemy.agility + '</p><p>Speed: ' + enemy.speed + '</p> </div>';
+        
+        
+        
 
 
     }
-
-
-
 
 }
